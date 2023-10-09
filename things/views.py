bbfa5dce968faf039e6686e20453b9e306d10ac0
Mohammadgulzar Habibullah <mohammedgulzar79@gmail.com>
2023-10-09 17:31:10 +0100

    Finish  exercise 1 and 2

--- things/views.py
+++ things/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 
-from .forms import Form
+from .forms import ThingForm
 
 
 def home(request):
-    form = Form()
+    form = ThingForm()
     return render(request, 'home.html', {'form': form})
